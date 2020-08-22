736214d800c88f1c41cc22b4cb7040a6219565c4
doyle-flutter <il4479@daum.net>
2020-08-23 02:15:42 +0900

    graphql update

--- lib/views/GraphQLPage.dart
+++ lib/views/GraphQLPage.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-
 import 'package:basicflutter/repo/ConnectNode.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
@@ -17,7 +17,7 @@ class _GraphQLPageState extends State<GraphQLPage> {
   void initState() {
     Future.microtask(() async{
       String query = "{hello(targetId:1){ id title}}";
-      http.Response _res = await http.get("http://192.168.0.2:3000/graphqlserver/data?query=$query");
+      http.Response _res = await http.get("${ConnectNode.END_POINT}/graphqlserver/data?query=$query");
       var _result = json.decode(_res.body);
       print(_result);
       setState(() {
@@ -34,13 +34,52 @@ class _GraphQLPageState extends State<GraphQLPage> {
         title: Text("Node.js GraphQL Server"),
         centerTitle: true,
       ),
-      body: this.data == null
-      ? Center(child: CircularProgressIndicator(),)
-      : ListView.builder(
-          itemCount: this.data.length,
-          itemBuilder: (BuildContext context, int index)
-            => Text(this.data[index].toString())
-        )
+      body: this.views(data: this.data)
     );
+  }
+
+  Widget views({List data}){
+    final HttpLink httpLink = HttpLink(uri: '${ConnectNode.END_POINT}/graphqlserver/data',);
+    final AuthLink authLink = AuthLink(getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',);
+    final Link link = authLink.concat(httpLink);
+
+    String readRepositories = "query{hello(targetId:1){id title}}";
+
+    if(data == null) return Center(child: CircularProgressIndicator(),);
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text("HTTP(S) : \n${this.data.toString()}")
+            )
+          ),
+          GraphQLProvider(
+            client: ValueNotifier(
+              GraphQLClient(
+                cache: InMemoryCache(),
+                link: link,
+              ),
+            ),
+            child: Expanded(
+              flex: 1,
+              child: Query(
+                options: QueryOptions(
+                  documentNode: gql(readRepositories),
+                  pollInterval: 10,
+                ),
+                builder: (QueryResult result, { VoidCallback refetch, FetchMore fetchMore }) {
+                  if(result.hasException) return Text(result.exception.toString());
+                  if(result.loading) return Text('Loading');
+                  return Center(child: Text("GraphQL Package : \n${result.data.toString()}"));
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
   }
 }
