8ed88c6648333164555d13a270207deafd1103a4
doyle <il4479@hanmail.net>
2021-01-03 01:43:13 +0900

    getxHttp

--- lib/views/GetXHttpPage.dart
+++ lib/views/GetXHttpPage.dart
@@ -26,10 +26,16 @@ class GetXHttp extends GetX.GetxController{
   final String _url = "https://raw.githubusercontent.com/doyle-flutter/basicflutter/master/lib/testJson.json";
 
   Future<void> connectServer() async{
-    final http.Response _res = await http.get(this._url);
-    final List _result = json.decode(_res.body);
-    // ignore: unnecessary_statements
-    this.data +_result;
+    if(data.length < 1 ){
+      final http.Response _res = await http.get(this._url);
+      final List _result = json.decode(_res.body);
+      // ignore: unnecessary_statements
+      this.data +_result;
+    }
+    else{
+      this.data.clear();
+    }
+
     return;
   }
 
